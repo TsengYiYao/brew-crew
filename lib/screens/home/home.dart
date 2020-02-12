bbba938cecac85dd9ae442d6701b2209a9fc7815
TsengYiYao <yiyatsen@gmail.com>
2020-02-12 14:49:37 +0800

    #18 - Firestore Streams - finished

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,12 +1,18 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:provider/provider.dart';
+
+import 'brew_list.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
       child: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
@@ -23,6 +29,7 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
+        body: BrewList(),
       ),
     );
   }
